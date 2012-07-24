0ea20f458d1804d8539839d196c13302644c1171
Sever Băneşiu <banesiu.sever@gmail.com>
2012-07-24 14:31:24 +0300

    Added meta and content keywords and dict-based document test.

--- resty/tests/test_documents.py
+++ resty/tests/test_documents.py
@@ -8,13 +8,15 @@ class TestJsonDocument(unittest.TestCase):
         from resty.documents import JsonDocument
         return JsonDocument
 
-    def _make_one(self, content):
-        return self._get_target()(json.dumps(content))
+    def _make_one(self, meta={}, content={}):
+        meta_copy = dict(('$%s' % key, value) for key, value in meta.items())
+        meta_copy.update(content)
+        return self._get_target()(json.dumps(meta_copy))
 
-    def _make_shortcut(self, content):
-        minimal_dict = {'$type': 'type', '$self': 'self'}
-        minimal_dict.update(content)
-        return self._make_one(minimal_dict)
+    def _make_shortcut(self, meta={}, content={}):
+        minimal_dict = {'type': 'type', 'self': 'self'}
+        minimal_dict.update(meta)
+        return self._make_one(minimal_dict, content)
 
     def test_empty_json(self):
         self.assertRaises(ValueError, self._make_one, {})
@@ -24,10 +26,10 @@ class TestJsonDocument(unittest.TestCase):
             content={'a': 'a', 'b': 'b'}
         )
         self.assertRaises(ValueError, self._make_one,
-            {'$type': 'type'}
+            meta={'type': 'type'}
         )
         self.assertRaises(ValueError, self._make_one,
-            {'$self': 'self'}
+            meta={'$self': 'self'}
         )
 
     def test_minimal_doc(self):
@@ -36,15 +38,26 @@ class TestJsonDocument(unittest.TestCase):
         self.assertEqual(d.self, 'self')
 
     def test_meta_attrs(self):
-        d = self._make_shortcut({'$a': 'a', '$b': 'b'})
+        d = self._make_shortcut(meta={'a': 'a', 'b': 'b'})
         self.assertTrue(hasattr(d.meta, 'a'))
         self.assertEqual(d.meta.a, 'a')
         self.assertTrue(hasattr(d.meta, 'b'))
         self.assertEqual(d.meta.b, 'b')
 
     def test_user_attrs(self):
-        d = self._make_shortcut({'a': 'a', 'b': 'b'})
+        d = self._make_shortcut(content={'a': 'a', 'b': 'b'})
         self.assertTrue(hasattr(d.user, 'a'))
         self.assertEqual(d.user.a, 'a')
         self.assertTrue(hasattr(d.user, 'b'))
         self.assertEqual(d.user.b, 'b')
+
+
+class TestDictDocument(TestJsonDocument):
+    def _get_target(self):
+        from resty.documents import DictDocument
+        return DictDocument
+
+    def _make_one(self, meta={}, content={}):
+        meta_copy = dict(('$%s' % key, value) for key, value in meta.items())
+        meta_copy.update(content)
+        return self._get_target()(meta_copy)
