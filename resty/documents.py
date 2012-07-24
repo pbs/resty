06e54b57bc83c5c15bded417bf4a3ef8c1ae0a9e
Angel Ramboi <angel.ramboi@gmail.com>
2012-07-24 18:06:23 +0300

    Removed unused class.

--- resty/documents.py
+++ resty/documents.py
@@ -48,31 +48,3 @@ class Collection(JsonDocument):
 class Service(JsonDocument):
     def __init__(self, data):
         super(Service, self).__init__(data)
-
-
-class dotdictify(dict):
-    marker = object()
-    def __init__(self, value=None):
-        if value is None:
-            pass
-        elif isinstance(value, dict):
-            for key in value:
-                self.__setitem__(key, value[key])
-        else:
-            raise TypeError, 'expected dict'
-
-    def __setitem__(self, key, value):
-        if isinstance(value, dict) and not isinstance(value, dotdictify):
-            value = dotdictify(value)
-        key = key.replace('$', '')
-        dict.__setitem__(self, key, value)
-
-    def __getitem__(self, key):
-        found = self.get(key, dotdictify.marker)
-        if found is dotdictify.marker:
-            found = dotdictify()
-            dict.__setitem__(self, key, found)
-        return found
-
-    __setattr__ = __setitem__
-    __getattr__ = __getitem__
