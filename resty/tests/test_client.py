48d4410dbf6b23eb371522622896283592d76f4f
Sever Băneşiu <banesiu.sever@gmail.com>
2012-07-26 16:04:09 +0300

    Added specialize test.

--- resty/tests/test_client.py
+++ resty/tests/test_client.py
@@ -1,6 +1,6 @@
 import unittest2 as unittest
 
-from resty.tests.mocks import MockHttpLoader
+from resty.tests.mocks import MockHttpLoader, MockDocument
 
 
 class TestClient(unittest.TestCase):
@@ -9,35 +9,66 @@ class TestClient(unittest.TestCase):
         from resty import Client
         return Client
 
-    def _make_one(self):
-        loader = MockHttpLoader()
-        loader.register('test://load_document/1/', 'type1', 'text1')
-        loader.register('test://load_document/2/', 'type2', 'text2')
+    def _make_one(self, loader):
         return self._get_target()(loader.load)
 
     def test_load_document(self):
 
-        class MockDocument(object):
+        loader = MockHttpLoader()
+        loader.register('test://load_document/1/', 'type1', 'text1')
+        loader.register('test://load_document/2/', 'type2', 'text2')
+
+        c = self._make_one(loader)
+
+        class Mock(object):
             def __init__(self, client, text):
                 self.client = client
                 self.text = text
 
-        class MockDocument1(MockDocument):
+        class Mock1(Mock):
             pass
 
-        class MockDocument2(MockDocument):
+        class Mock2(Mock):
             pass
 
-        c = self._make_one()
-        c.register_document_parser('type1', MockDocument1)
-        c.register_document_parser('type2', MockDocument2)
+        c.register_document_parser('type1', Mock1)
+        c.register_document_parser('type2', Mock2)
 
         d = c.load_document('test://load_document/1/')
         self.assertEqual(d.client, c)
         self.assertEqual(d.text, 'text1')
-        self.assertTrue(isinstance(d, MockDocument1))
+        self.assertTrue(isinstance(d, Mock1))
 
         d = c.load_document('test://load_document/2/')
         self.assertEqual(d.client, c)
         self.assertEqual(d.text, 'text2')
-        self.assertTrue(isinstance(d, MockDocument2))
+        self.assertTrue(isinstance(d, Mock2))
+
+    def test_specialize(self):
+
+        loader = MockHttpLoader()
+        c = self._make_one(loader)
+
+        class Mock(object):
+            def __init__(self, doc):
+                self.doc = doc
+
+        class Mock1(Mock):
+            pass
+
+        class Mock2(Mock):
+            pass
+
+        c.register_document('type1', Mock1)
+        c.register_document('type2', Mock2)
+
+        doc1 = MockDocument(meta={'type': 'type1'})
+        doc2 = MockDocument(meta={'type': 'type2'})
+
+        s1 = c.specialize(doc1)
+        self.assertTrue(isinstance(s1, Mock1))
+        self.assertEqual(s1.doc, doc1)
+
+        s2 = c.specialize(doc2)
+        self.assertTrue(isinstance(s2, Mock2))
+        self.assertEqual(s2.doc, doc2)
