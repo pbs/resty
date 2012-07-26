423f6710f30e0f8ae98b7c63b75e897bae7e35b4
Angel Ramboi <angel.ramboi@gmail.com>
2012-07-26 16:18:02 +0300

    Http loader function.

--- resty/__init__.py
+++ resty/__init__.py
@@ -1,3 +1,5 @@
+import requests
+
 class Client(object):
     def __init__(self, loader):
         self.loader = loader
@@ -5,19 +7,24 @@ class Client(object):
         self.register = {}
 
     def load_document(self, uri):
-        type, text = self.loader(uri)
-        doc = self.register_parser[type]
+        type_, text = self.loader(uri)
+        doc = self.register_parser[type_]
         return doc(self, text)
 
-    def register_document_parser(self, type, class_):
-        self.register_parser[type] = class_
+    def register_document_parser(self, type_, class_):
+        self.register_parser[type_] = class_
 
-    def register_document(self, type, class_):
-        self.register[type] = class_
+    def register_document(self, type_, class_):
+        self.register[type_] = class_
 
     def specialize(self, class_):
-        return self.register[class_.type](class_)
+        return self.register[class_.type_](class_)
 
     def load(self, uri):
         d = self.load_document(uri)
         return d.specialize()
+
+
+def http_loader(uri):
+    response = requests.get(uri)
+    return response.headers.get('content-type'), response.text
