1b150f704efaab290db9bfae4fe8ddd993ea80e1
Sever Băneşiu <banesiu.sever@gmail.com>
2013-03-27 11:06:10 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='resty',
-    version='0.1',
+    version='0.1.1',
     description='PBS API consumer library',
     author='TPG CORE Services Team',
     author_email='tpg-pbs-coreservices@threepillarglobal.com',
@@ -12,5 +12,5 @@ setup(
     include_package_data=True,
     install_requires=['requests'],
     setup_requires=['unittest2'],
-    test_suite = 'unittest2.collector',
+    test_suite='unittest2.collector',
 )
